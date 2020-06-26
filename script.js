4a261ac5917412f5635d3a172d6327f9b46ae89f
Ruben <64035906+rubenkretek@users.noreply.github.com>
2020-06-26 14:00:29 +0100

    added end points

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-
-
-// question 1
-
-// if 'yes' button is clicked run add show function
-// -- listen to click of button
-// -- if value is 'yes' show question 2
-// -- if 'no' button is clicked show question 2b
-
-// create function which toggles .show
 let button1yes = document.getElementById('1yes');
 let button1no = document.getElementById('1no');
+let button2b = document.getElementById('2b-submit');
+
+let borrowingAmount = 0;
 
 const question1 = document.getElementById('question-1');
 const question2 = document.getElementById('question-2');
@@ -17,9 +10,6 @@ const question2b = document.getElementById('question-2b');
 const question3 = document.getElementById('question-3');
 const question4 = document.getElementById('question-4');
 const question5 = document.getElementById('question-5');
-
-
-
 
 
 function showQuestion(question) {
@@ -37,12 +27,28 @@ function disableQuestion(question) {
 }
 
 
-
 button1yes.addEventListener('click', event => {
     showQuestion(question2); 
     disableQuestion(question1);    
 });
 
+
+
 button1no.addEventListener('click', event => {
     showQuestion(question2b);
+});
+
+
+
+button2b.addEventListener('click', event => {
+    let inputField = document.getElementById('2b-input').value;
+    borrowingAmount = inputField;
+
+    if (borrowingAmount > 1999999 && borrowingAmount < 5000000) {
+        console.log("ct sme");   
+    } else if (borrowingAmount > 5000000 && borrowingAmount < 10000000){
+        console.log('ct executive');        
+    } else if (borrowingAmount > 10000000) {
+        console.log('ct corporate');        
+    }
 });
