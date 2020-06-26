36a2ad5379f5a36b5c34ca109bee4787bcff6d0b
Ruben <64035906+rubenkretek@users.noreply.github.com>
2020-06-26 14:50:31 +0100

    Created functionality for question 2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let button1yes = document.getElementById('1yes');
 let button1no = document.getElementById('1no');
+let button2 = document.getElementById('2-submit');
 let button2b = document.getElementById('2b-submit');
 
 let borrowingAmount = 0;
@@ -11,22 +12,48 @@ const question3 = document.getElementById('question-3');
 const question4 = document.getElementById('question-4');
 const question5 = document.getElementById('question-5');
 
+const anwserSME = document.getElementById('answer-sme');
+const anwserExecutive = document.getElementById('answer-executive');
+const anwserCorporate = document.getElementById('answer-corporate');
+const anwserNA = document.getElementById('answer-na');
 
 function showQuestion(question) {
     question.classList.add("show");
 };
 
+function showAnswer(answer) {
+    answer.classList.add("show");
+};
+
+//Checks amount user wants to borrow and then shows the correct message
+function checkAmount(amount) {
+    if (borrowingAmount >= 2000000 && borrowingAmount < 5000000) {
+        console.log("ct sme");
+        showAnswer(anwserSME);   
+    } else if (borrowingAmount >= 5000000 && borrowingAmount < 10000000){
+        console.log('ct executive');        
+        showAnswer(anwserExecutive);
+    } else if (borrowingAmount >= 10000000) {
+        console.log('ct corporate');
+        showAnswer(anwserCorporate);        
+    }
+};
+
+
 //disables buttons and adds "disable" to the question
 function disableQuestion(question) {
     var buttons = question.querySelectorAll('button');    
+    var inputs = question.querySelectorAll('input');
     buttons.forEach(element => {
-        element.disabled = true;
-        
+        element.disabled = true;    
+    });
+    inputs.forEach(element => {
+        element.disabled = true;    
     });
     question.classList.add("disable");
 }
 
-
+//Question 1 -----
 button1yes.addEventListener('click', event => {
     showQuestion(question2); 
     disableQuestion(question1);    
@@ -36,19 +63,29 @@ button1yes.addEventListener('click', event => {
 
 button1no.addEventListener('click', event => {
     showQuestion(question2b);
+    disableQuestion(question1); 
 });
 
+//Question 2 ------
+button2.addEventListener('click', event => {
+    let inputField = document.getElementById('2-input').value;
+    borrowingAmount = inputField;
 
+    if (borrowingAmount < 250000) {
+        showAnswer(anwserNA);   
+    } else if (borrowingAmount >= 250000 && borrowingAmount <= 4000000){
+        console.log('carry on with form');        
+    } else if (borrowingAmount > 4000000) {
+        checkAmount(borrowingAmount);        
+    }
 
+    disableQuestion(question2); 
+});
+
+//Question 2b -----
 button2b.addEventListener('click', event => {
     let inputField = document.getElementById('2b-input').value;
     borrowingAmount = inputField;
-
-    if (borrowingAmount > 1999999 && borrowingAmount < 5000000) {
-        console.log("ct sme");   
-    } else if (borrowingAmount > 5000000 && borrowingAmount < 10000000){
-        console.log('ct executive');        
-    } else if (borrowingAmount > 10000000) {
-        console.log('ct corporate');        
-    }
+    checkAmount(borrowingAmount);
+    disableQuestion(question2b); 
 });
