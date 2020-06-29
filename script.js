ce41946eeefde3306d836a9ae9bd828a3bf09555
Ruben <64035906+rubenkretek@users.noreply.github.com>
2020-06-29 14:57:00 +0100

    First Prototype, all logic added and works as it should

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ let button1yes = document.getElementById('1yes');
 let button1no = document.getElementById('1no');
 let button2 = document.getElementById('2-submit');
 let button2b = document.getElementById('2b-submit');
+let button3yes = document.getElementById('3yes');
+let button3no = document.getElementById('3no');
+let button4yes = document.getElementById('4yes');
+let button4no = document.getElementById('4no');
+let button5yes = document.getElementById('5yes');
+let button5no = document.getElementById('5no');
 
 let borrowingAmount = 0;
 
@@ -18,6 +24,10 @@ const anwserSME = document.getElementById('answer-sme');
 const anwserExecutive = document.getElementById('answer-executive');
 const anwserCorporate = document.getElementById('answer-corporate');
 const anwserNA = document.getElementById('answer-na');
+const anwserTooLow = document.getElementById('answer-too-low');
+const anwserRaiseValuation = document.getElementById('answer-raise-valuation');
+
+
 
 function showQuestion(question) {
     question.classList.add("show");
@@ -29,7 +39,10 @@ function showAnswer(answer) {
 
 //Checks amount user wants to borrow and then shows the correct message
 function checkAmount(amount) {
-    if (borrowingAmount >= 2000000 && borrowingAmount < 5000000) {
+    if (borrowingAmount < 2000000) {
+        console.log("too low");
+        showAnswer(anwserTooLow);   
+    } else if (borrowingAmount >= 2000000 && borrowingAmount < 5000000) {
         console.log("ct sme");
         showAnswer(anwserSME);   
     } else if (borrowingAmount >= 5000000 && borrowingAmount < 10000000){
@@ -42,7 +55,7 @@ function checkAmount(amount) {
 };
 
 
-//disables buttons and adds "disable" to the question
+//disables buttons and adds "disable" class to the question
 function disableQuestion(question) {
     var buttons = question.querySelectorAll('button');    
     var inputs = question.querySelectorAll('input');
@@ -68,7 +81,7 @@ button1no.addEventListener('click', event => {
     disableQuestion(question1); 
 });
 
-//Question 2 ------
+//Question 2 (and amount shown on question 3)------
 button2.addEventListener('click', event => {
     let inputField = document.getElementById('2-input').value;
     borrowingAmount = inputField;
@@ -92,4 +105,41 @@ button2b.addEventListener('click', event => {
     borrowingAmount = inputField;
     checkAmount(borrowingAmount);
     disableQuestion(question2b); 
+});
+
+//Question 3 ------
+button3yes.addEventListener('click', event => {
+    showQuestion(question4); 
+    disableQuestion(question3);    
+});
+
+
+
+button3no.addEventListener('click', event => {
+    checkAmount(borrowingAmount);
+    disableQuestion(question3);
+});
+
+
+//Question 4 ------
+button4yes.addEventListener('click', event => {
+    showQuestion(question5);
+    disableQuestion(question4);    
+});
+
+button4no.addEventListener('click', event => {
+    showAnswer(anwserRaiseValuation);
+    disableQuestion(question4);
+});
+
+
+//Question 5 ------
+button5yes.addEventListener('click', event => {
+    checkAmount(borrowingAmount);
+    disableQuestion(question5);    
+});
+
+button5no.addEventListener('click', event => {
+    showAnswer(anwserRaiseValuation);
+    disableQuestion(question5);
 });
